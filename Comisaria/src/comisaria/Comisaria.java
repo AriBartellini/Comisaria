196e5efb2425404dce12d38a13f3007fe7d15b64
AriBartellini <aribartellini@gmail.com>
2023-09-08 13:34:08 -0300

    Clases Descriptas

--- Comisaria/src/comisaria/Comisaria.java
+++ Comisaria/src/comisaria/Comisaria.java
@@ -15,7 +15,5 @@ public class Comisaria {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
-    
 }
